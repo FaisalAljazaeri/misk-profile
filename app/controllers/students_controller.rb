eb3d48f7d1e5675baca29eecbad1b16dd7609ba7
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-04-02 22:53:09 +0300

    Add Student Show Action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,20 @@
 class StudentsController < ApplicationController
+    # Find student by ID and save it in @student, for the actions that require it
+    before_action :find_student, only: [:show]
+
     def index
+        # Render 'index' view and pass it all students
         @students = Student.all
     end
+
+    def show
+        # Render 'show' view and send it @student found by id
+        @student 
+    end
+
+    private
+        def find_student
+            # Find student by id from the params
+            @student = Student.find(params[:id])
+        end
 end
