379811c324a90d228caf884de75edc84cce60b3c
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-04-03 16:50:07 +0300

    Add Skill Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_222331) do
+ActiveRecord::Schema.define(version: 2020_04_03_134911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
