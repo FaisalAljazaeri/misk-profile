a244064d306a10556763194ba3bc1565010f0607
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-04-03 17:20:00 +0300

    Join Student and Skill Model Through StudentSkill Model

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,7 @@ class Student < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :student_skills
+  has_many :skills, through: :student_skills
 end
