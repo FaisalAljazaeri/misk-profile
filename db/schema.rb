f69c91cb443a0bc9df28dfa90b70de5b24be9050
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-04-04 20:39:47 +0300

    Generate Project Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_161320) do
+ActiveRecord::Schema.define(version: 2020_04_04_173911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2020_04_04_161320) do
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.string "abbreviation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "link"
+    t.string "description"
+    t.string "img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
