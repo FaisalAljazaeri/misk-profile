1971fd6fcb08796a9d5e3372703b97d255c57863
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-04-04 19:59:03 +0300

    Include Summary and Course in Student Sign-up Form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 
     protected
         def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :img])
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :img, :course_id, :summary])
         end
 end
