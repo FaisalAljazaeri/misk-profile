14ad6cb5645abf2b94032724782b1fceb1d82af5
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-04-07 14:12:57 +0300

    Add github_link to Project Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_025848) do
+ActiveRecord::Schema.define(version: 2020_04_07_111112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_04_07_025848) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "student_id"
+    t.string "github_link"
     t.index ["student_id"], name: "index_projects_on_student_id"
   end
 
